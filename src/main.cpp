03e99a5d81d4d78e111bba6a3952978f11553547
Color <eastopstudio@163.com>
2026-02-28 19:33:51 +0800

    Add remaining files: CMake tweaks, main.cpp changes, assets and resources

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <QApplication>
 #include <QMessageBox>
 #include <QDebug>
+#include <QIcon>
 #include "SystemDataProvider.h"
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    
+    // 设置应用程序图标（从资源文件加载）
+    app.setWindowIcon(QIcon(":/logo.ico"));
     
     qDebug() << "Starting NeoFetch Pro...";
     
@@ -16,6 +20,8 @@ int main(int argc, char *argv[])
     MainWindow window(&systemData);
     qDebug() << "MainWindow created";
     
+    // 窗口图标（可选，因为应用程序图标已设置）
+    window.setWindowIcon(QIcon(":/logo.ico"));
     window.show();
     qDebug() << "Window shown";
     
